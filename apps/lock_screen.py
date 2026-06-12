709770483acf86f3b29c093732253bc93588e2ce
alfred <masteraj546@gmail.com>
2026-06-12 16:07:26 -0400

    refactor lock screen to be menu-driven, with PIN entry on unlock

--- apps/lock_screen.py
+++ apps/lock_screen.py
@@ -5,9 +5,13 @@ import win32con
 import psutil
 from core.app_base import SoftApp
 from core.audio_player import AudioPlayer
+from core.menu import MenuNode, MenuSystem
 
 BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 UNLOCK_SOUND = os.path.join(BASE, 'sounds', 'unlock.mp3')
+
+STATE_MENU = 0
+STATE_PIN = 1
 
 class LockScreenApp(SoftApp):
     def __init__(self, manager, window, success_callback):
@@ -21,7 +25,23 @@ class LockScreenApp(SoftApp):
         else:
             self.account = {}
         self.input_pin = ""
-        self.state = "locked_info"
+        self.state = STATE_MENU
+        self.menu = None
+        self._build_menu()
+
+    def _build_menu(self):
+        root = MenuNode("Locked")
+        root.add_child(MenuNode("Time and Date", self._announce_time))
+        root.add_child(MenuNode("Battery Level", self._announce_battery))
+        root.add_child(MenuNode("Unlock", self._start_pin_entry))
+        self.menu = MenuSystem(root, self.speak)
+
+    def _announce_time(self):
+        self.speak(self._time_info())
+
+    def _announce_battery(self):
+        info = self._battery_info()
+        self.speak(info if info else "Battery information unavailable.")
 
     def _battery_info(self):
         try:
@@ -41,21 +61,37 @@ class LockScreenApp(SoftApp):
         return f"{time_str}. {date_str}. "
 
     def on_focus(self):
-        info = self._time_info() + self._battery_info() + "Press Enter to unlock."
+        self.state = STATE_MENU
         self.window.update_text("Locked")
-        self.speak(info)
+        self.speak("Lock Screen. " + self.menu.get_current_item().title)
+
+    def _start_pin_entry(self):
+        self.state = STATE_PIN
+        self.input_pin = ""
+        self.speak("Enter PIN.")
+        self.window.update_text("PIN:")
 
     def on_key(self, vk):
-        if self.state == "locked_info":
-            if vk == win32con.VK_RETURN:
-                self.state = "pin_entry"
-                self.speak("Enter PIN.")
-                self.window.update_text("PIN:")
-            elif vk == win32con.VK_ESCAPE:
+        if self.state == STATE_MENU:
+            if vk == win32con.VK_ESCAPE:
                 self.exit_app()
-            return
+                return
 
-        if self.state == "pin_entry":
+            if vk in (win32con.VK_SPACE, win32con.VK_DOWN):
+                self.menu.next()
+            elif vk in (win32con.VK_BACK, win32con.VK_UP):
+                self.menu.previous()
+            elif vk == win32con.VK_RETURN:
+                self.menu.select()
+            elif 0x41 <= vk <= 0x5A:
+                self.menu.first_letter_nav(chr(vk))
+            
+            if self.menu:
+                item = self.menu.get_current_item()
+                title = item.title if item else self.menu.current_node.title
+                self.window.update_text(title)
+
+        elif self.state == STATE_PIN:
             self._handle_pin(vk)
 
     def _handle_pin(self, vk):
@@ -78,13 +114,17 @@ class LockScreenApp(SoftApp):
                 text = "*" * len(self.input_pin) if self.input_pin else "PIN:"
                 self.window.update_text(text)
             else:
-                self.state = "locked_info"
+                self.state = STATE_MENU
                 self.speak("Cancelled.")
                 self.window.update_text("Locked")
+                if self.menu:
+                    self.menu.announce_current()
         elif vk == win32con.VK_ESCAPE:
-            self.state = "locked_info"
+            self.state = STATE_MENU
             self.speak("Cancelled.")
             self.window.update_text("Locked")
+            if self.menu:
+                self.menu.announce_current()
 
     def _play_unlock(self):
         if os.path.exists(UNLOCK_SOUND):
