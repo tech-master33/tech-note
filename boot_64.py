27bdecf2522c4325ce2bd11f0b0e85196670a18e
alfred <masteraj546@gmail.com>
2026-06-12 20:07:08 -0400

    clean up redundant click sound logic in boot_64.py

--- boot_64.py
+++ boot_64.py
@@ -128,12 +128,7 @@ class BrailleNoteApp:
         self.menu_root = build_braillenote_menu(
             self.synth, self.window, self.launch_app, self._reset_and_restart
         )
-        click_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sounds', 'clicked.ogg')
-        self._click_player = AudioPlayer()
-        def play_click():
-            if os.path.exists(click_path):
-                self._click_player.play_file(click_path)
-        self.menu = MenuSystem(self.menu_root, self.synth.speak, play_sound=play_click)
+        self.menu = MenuSystem(self.menu_root, self.synth.speak)
         self.synth.speak("Main Menu")
 
     def launch_app(self, app_class):
